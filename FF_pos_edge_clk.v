4c93238316492c133bc420337aa5fc623e7e5933
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-03-20 20:10:28 +0530

    Flip Flop Positive Edge Clock

---
+++ FF_pos_edge_clk.v
@@ -0,0 +1,9 @@
+module flop(clk,d,q);
+	input clk,d;
+	output q;
+	reg q;
+	always@(posedge clk)
+	begin
+		q<=d;
+	end
+endmodule
---
+++ FF_pos_edgeclk_asynchrousset_clkenable.v
@@ -0,0 +1,13 @@
+module ff(clk,d,ce,pre,q);
+	input clk,ce,pre;
+	input [3:0] d;
+	output [3:0] q;
+	reg [3:0] q;
+	always @(posedge clk or posedge pre)
+	begin
+		if(pre)
+			q<=4'b1111;
+		else if (ce)
+			q<=d;
+	end
+endmodule
