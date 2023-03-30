93586de511dffcfd849baa754a24eb558634388c
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-03-30 23:32:22 +0530

    Create up_counterr.v

---
+++ Counters/up_counterr.v
@@ -0,0 +1,11 @@
+module up_counterr(input clk, reset, output[3:0] counter);
+    reg [3:0] counter_up;
+    always @(posedge clk or posedge reset)
+    begin
+    if(reset)
+      counter_up <= 4'd0;
+    else
+      counter_up <= counter_up + 4'd1;
+    end 
+    assign counter = counter_up;
+endmodule
