beaf48ea3633b17d416e3b85479d412ac8adbb68
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-04-19 22:28:06 +0530

    Update encoder_8_three.v

--- Encoder/encoder_8_three.v
+++ Encoder/encoder_8_three.v
@@ -9,13 +9,13 @@ module encoder_8_three(input en, input a, output op)
 	begin
 		if(en==1)
 		begin
-			if(i[7]==1) y=3'b111;
-			else if(i[6]==1) op=3'b110;
-			else if(i[5]==1) op=3'b101;
-			else if(i[4]==1) op=3'b100;
-			else if(i[3]==1) op=3'b011;
-			else if(i[2]==1) op=3'b010;
-			else if(i[1]==1) op=3'b001;
+			if(a[7]==1) y=3'b111;
+			else if(a[6]==1) op=3'b110;
+			else if(a[5]==1) op=3'b101;
+			else if(a[4]==1) op=3'b100;
+			else if(a[3]==1) op=3'b011;
+			else if(a[2]==1) op=3'b010;
+			else if(a[1]==1) op=3'b001;
 			else
 			op=3'b000;
 		end
