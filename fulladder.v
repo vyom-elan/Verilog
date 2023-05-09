210de216db49eb1966ff9f263e1e7047b65cc62c
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-05-10 00:02:13 +0530

    Update fulladder.v

--- fulladder.v
+++ fulladder.v
@@ -1,6 +1,6 @@
 module fulladder(a,b,ic,o,oc);
-  input a,b,ic; htjdf
-  output o,oc;
+  input a,b,ic;     //inputs  
+  output o,oc;      //outputs
   assign o = (~ic & ((a & ~b) | (~a & b)) ) | (ic & ~((a & ~b) | (~a & b)) );   
   assign oc = (a & b) | (b & ic) | (ic & a); 
 endmodule
