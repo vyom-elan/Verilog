0902a04ec6138b7ccb300dfb385847bd79ec255c
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-06-12 13:23:25 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,17 +1,15 @@
-//ALU 4-bit
-
-module ALU(x,y,a,b,opcode);
-input [3:0]a,b,opcode;
+module ALU(x,y,a,b,op);
+input [3:0]a,b,op;
 output [3:0]x,y;
-always @(a,b,opcode)
-	case(opcode)
+always @(a,b,op)
+	case(op)
 		4'b0000: x[0]= |a;
 		4'b0001: x[0]= &a;
 		4'b0010: x[0]= ^a;
 		4'b0011: x = a&b;
 		4'b0100: x = a|b;
 		4'b0101: x = a^b;
-		4'b0110: x[0]=a>b;
+		4'b0110: x[0]=a>b;				//for imlementing basic comparisons
 		4'b0111: x[0]=a<b;
 		4'b1000: x[0]=!a;
 		4'b1001: x[0]= a==b;
@@ -21,6 +19,5 @@ always @(a,b,opcode)
 		4'b1101: {y,x}=a>>b;
 		4'b1110: {y,x}=a<<b;
 		4'b1111: x = ~a;
-		default:$display("error");
 	endcase
 endmodule
