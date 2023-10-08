00eff646549d1185baa9abf2c00fc130294ce677
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-10-08 17:15:40 +0530

    Create Bi_Directional_Shift_Register.v

---
+++ Shift Register/Bi_Directional_Shift_Register.v
@@ -0,0 +1,14 @@
+module bdShiftReg#(parameter MSB=8)(input d, input clk, input en, input dir, input rstn, output reg[MSB-1:0] out);
+  always @ (posedge clk)
+    if(!rstn)
+      out<=0;
+  else begin
+    if(en)
+      case(dir)
+        0: out<={out[MSB-2:0],d};
+        1: out<={d,out[MSB-1:1]};
+      endcase
+    else
+      out<=out;
+  end
+endmodule
