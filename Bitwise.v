9e22fbccf1c6473248846b74f52ff971f3cd0700
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-01-22 20:12:45 +0530

    Create Bitwise.v

---
+++ Bitwise.v
@@ -0,0 +1,8 @@
+module gates(input [3:0] a, b,
+             output [3:0] y1, y2, y3, y4, y5);
+  assign y1 = a & b; // AND
+  assign y2 = a | b; // OR
+  assign y3 = a ^ b; // XOR
+  assign y4 = ~(a & b); // NAND
+  assign y5 = ~(a | b); // NOR
+endmodule
