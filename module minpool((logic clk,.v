f085b52025087d4dcbb0818b876d9cd26548bbf5
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-03-29 23:42:25 +0530

    Add files via upload

---
+++ module minpool((logic clk,.v
@@ -0,0 +1,10 @@
+module minpool((logic clk,
+          logic rst,
+		  logic [7:0] ip_a, //input-
+		  logic [7:0] ip_b, //logicb	
+		  logic [7:0] ip_c, //logicc
+		  logic [7:0] ip_d, //logicd
+		  output reg signed [7:0] op, //output
+          output reg PoolComplete);
+
+endmodule
